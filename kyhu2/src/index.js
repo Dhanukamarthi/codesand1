287a89e971443d8c010751c8c0eabd2e25265e00
Dhanush <dhanu2kamrthi@gmail.com>
2021-12-16 19:52:00 +0530

    my messsage

---
+++ kyhu2/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Family members</h1>
+    <ul>
+      <li>suseela</li>
+      <li>padmavathi</li>
+      <li>Dhanush</li>
+
+      <li>sreenivasulu</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
